48992f26adfbba609c8dbed801a8d4810b31ab42
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-23 18:13:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'u2873136-6e865498164b08a6de140d5f', //  github图床 API key
-    //'m797638073-924f5efed86446cce1c95ae1', // 周润发的博客 API key
+    'ur2873136-d272d74aefdd8fccef071883', // 周润发的博客 API key
     //'m797713213-1c83620d56db599960ad0244', // 周润发的公益订阅 API key
     //'m797713331-835664f191b589688f122e00', // 周润发的公益订阅器 API key
   ],
